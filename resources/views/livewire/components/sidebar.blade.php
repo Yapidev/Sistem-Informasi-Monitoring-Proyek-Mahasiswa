a587f7034f59e710f21abf3165f30c06c3ba051e
Yapidev <88489723+Yapidev@users.noreply.github.com>
2025-04-22 18:16:01 +0700

    update

--- resources/views/livewire/components/sidebar.blade.php
+++ resources/views/livewire/components/sidebar.blade.php
@@ -2,10 +2,8 @@
     <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
             <a href="#" class="text-nowrap logo-img" wire:navigate>
-                <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/dark-logo.svg"
-                    class="dark-logo" width="180" alt="" />
-                <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/dark-logo.svg"
-                    class="light-logo" width="180" alt="" />
+                <img src="{{ asset('assets/images/mipens_logo.svg') }}" class="dark-logo" width="180" alt="" />
+                <img src="{{ asset('assets/images/mipens_logo.svg') }}" class="light-logo" width="180" alt="" />
             </a>
             <div class="close-btn d-lg-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                 <i class="ti ti-x fs-8 text-muted"></i>
